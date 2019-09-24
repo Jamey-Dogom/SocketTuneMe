28166939571a50fe55c2ae0be1aeaf5565827e0c
Hector Lopez <thebomblopez@gmail.com>
2019-09-23 19:23:51 -0700

    moved home component

--- public/src/app/app.component.ts
+++ public/src/app/app.component.ts
@@ -12,69 +12,15 @@ import { Socket } from 'ngx-socket-io';
 
 
 export class AppComponent implements OnInit {
-  newSong: any
-  SongId: ""
-  title = 'public';
-  allSongs = []
-  songs = false
-  userIP = null;
+
 
 
   constructor(
     private _httpService: HttpService,
     private _socket: Socket
-  ) {
-    this.newSong = { id: "" }
-  }
+  ) {}
 
   ngOnInit() {
-    //Called after the constructor, initializing input properties, and the first call to ngOnChanges.
-    //Add 'implements OnInit' to the class.
-    this.logIn();
-    console.log(this.userIP);
+
   }
-
-  logIn() {
-    this._socket.on('logIn', (data) => {
-      this.userIP = data.ip.address;
-    })
-  }
-  // Song Liked 
-  // update the song object's like field
-  //- re-sort {{allSongs}} array by # of Likes
-
-  // Next Song
-  // method for determining when a song ends
-  // plays the next
-
-
-
-  onSubmit() {
-    // Submission of FIRST new song
-    if (this.songs) {
-      this.allSongs.push(this.newSong.link)
-      this.newSong = { id: "" }
-    }
-    // Submission of songs after the first
-    else {
-      var str = this.newSong.link;
-      var array = str.split(/[=&]+/);
-      this.SongId = array[1];
-      this.newSong = { id: "" }
-      this.songs = true
-    }
-
-    // this.SongId = this.newSong.id
-    // // Code to send off the form data (this.newTask) to the Service
-    // console.log(this.newShow)
-    // let observable = this._httpService.addShow(this.newShow);
-    // // Reset this.newTask to a new, clean object.
-    // observable.subscribe(data => {
-    //   this.allShows = []
-    //   console.log('gottem', data)
-    //   this.newShow = { title: "", description: "" }
-    // })
-    // this.getAllShows();
-  }
-
 }
